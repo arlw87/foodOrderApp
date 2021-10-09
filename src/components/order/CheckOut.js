75deab6436e28ea26e082dcf5e7ae2c879761645
arlw87 <arlw87@googlemail.com>
2021-10-09 13:12:01 +0100

    Added form validation for all the input element in form

--- src/components/order/CheckOut.js
+++ src/components/order/CheckOut.js
@@ -1,8 +1,21 @@
 import Styles from '../order/CheckOut.module.css';
 import useInput from '../hooks/useInput';
 
-const isNameValidate = (name) => {
+const isNameValidFn = (name) => {
     return name.trim().length > 0;
+}
+
+const isStreetValidFn = (name) => {
+    return name.trim().length > 0;
+}
+
+const isPostCodeValidFn = (postcode) => {
+    const regex_exp = /[A-Z]{1,2}[0-9][0-9A-Z]?\s?[0-9][A-Z]{2}/g;
+    return postcode.match(regex_exp);
+}
+
+const isCityValidFn = (city) => {
+    return city.trim().length > 0 && (city.charAt(0) === city.charAt(0).toUpperCase());
 }
 
 const CheckOut = (props) => {
@@ -11,10 +24,37 @@ const CheckOut = (props) => {
         value: nameValue,
         handleInput: handleNameInput,
         handleTouched: handleNameBlur,
-        reset: restName,
+        reset: resetName,
         isValid: isNameValid,
         isError: hasNameError
-    } = useInput(isNameValidate);
+    } = useInput(isNameValidFn);
+
+    const {
+        value: streetValue,
+        handleInput: handleStreetInput,
+        handleTouched: handleStreetBlur,
+        reset: resetStreet,
+        isValid: isStreetValid,
+        isError: hasStreetError
+    } = useInput(isStreetValidFn);
+
+    const {
+        value: postCodeValue,
+        handleInput: handlePostCodeInput,
+        handleTouched: handlePostCodeBlur,
+        reset: resetPostCode,
+        isValid: isPostCodeValid,
+        isError: hasPostCodeError
+    } = useInput(isPostCodeValidFn);
+
+    const {
+        value: cityValue,
+        handleInput: handleCityInput,
+        handleTouched: handleCityBlur,
+        reset: resetCity,
+        isValid: isCityValid,
+        isError: hasCityError
+    } = useInput(isCityValidFn);
 
     return(
         <form>
@@ -23,17 +63,20 @@ const CheckOut = (props) => {
                 <input type='text' id='name' value ={nameValue} onChange={handleNameInput} onBlur={handleNameBlur}></input>
                 {hasNameError?<p>Please enter name</p>:''}
             </div>
-            <div>
+            <div className={hasStreetError?Styles.errorInput:''}> 
                 <label htmlFor='street'>Street</label>
-                <input type='text' id='street'></input>
+                <input type='text' id='street' value={streetValue} onChange={handleStreetInput} onBlur={handleStreetBlur}></input>
+                {hasStreetError?<p>Please enter your street name</p>:''}
             </div>
-            <div>
+            <div className={hasPostCodeError?Styles.errorInput:''}>
                 <label htmlFor='postcode'>Post Code</label>
-                <input type='text' id='postcode'></input>
+                <input type='text' id='postcode' value={postCodeValue} onChange={handlePostCodeInput} onBlur={handlePostCodeBlur}></input>
+                {hasPostCodeError?<p>Please enter a valid Postcode</p>:''}
             </div>
-            <div>
+            <div className={hasCityError?Styles.errorInput:''}>
                 <label htmlFor='city'>City</label>
-                <input type='text' id='city'></input>
+                <input type='text' id='city' value={cityValue} onBlur={handleCityBlur} onChange={handleCityInput}></input>
+                {hasCityError?<p>Please enter a valid City (Upper case first letter)</p>:''}
             </div>
         </form>
     )
